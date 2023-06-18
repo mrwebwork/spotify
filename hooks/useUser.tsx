a13476f1792284500b32a7ea43c99974cfd95112
mrwebwork <tech@allanswebwork.info>
2023-06-18 17:33:54 -0500

    Export useUser hook

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -2,7 +2,7 @@
 import { User } from "@supabase/auth-helpers-nextjs";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
 import { UserDetails, Subscription } from "@/types";
-import { useState, createContext, useEffect } from "react";
+import { useState, createContext, useEffect, useContext } from "react";
 
 //* Define a TypeScript type for the user context
 type UserContextType = {
@@ -91,4 +91,12 @@ export const MyUserContextProvider = (props: Props) => {
     };
 
     return <UserContext.Provider value={value} {...props}/>
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext)
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider')
+    }
+    return context;
 }
