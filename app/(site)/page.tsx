1262b4ab89035986b447235d836699ddb7ebbbbf
mrwebwork <tech@allanswebwork.info>
2023-06-18 15:38:16 -0500

    Complete Spotify layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,31 @@
 import { Header } from "@/components/Header"
+import { ListItem } from "@/components/ListItem"
 
 export default function Home() {
   return (
    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden">
       <Header>
-        Header
+        <div className="mb-2">
+          <h1 className="text-white text-3xl text-semibold">Welcome back</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem 
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
+        </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+          <div className="flex justify-between items-center">
+              <h1 className="text-white text-2xl font-semibold">
+                Newest Songs
+              </h1>
+          </div>
+          <div>
+            List of Songs!
+          </div>
+      </div>
    </div>
   )
 }
