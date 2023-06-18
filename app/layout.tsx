09c094391c3bd8d160d13ff09fa3c8995bc796ca
mrwebwork <tech@allanswebwork.info>
2023-06-18 13:51:12 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
