b6502405c62edcad7a1076ad29173d9358a0ea8b
mrwebwork <tech@allanswebwork.info>
2023-06-19 22:20:12 -0500

    Enable login and github auth

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useEffect } from "react";
+
 import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react"
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
@@ -14,6 +16,13 @@ export const AuthModal = () => {
     const router = useRouter();
     const { session } = useSessionContext();
     const { onClose, isOpen } = useAuthModal();
+
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            onClose();
+        }
+    }, [session, router, onClose]);
 
     const onChange = (open: boolean) => {
         if (!open) {
@@ -31,7 +40,7 @@ export const AuthModal = () => {
             <Auth 
             theme="dark"
             magicLink
-            providers={['github', 'google', 'linkedin']}
+            providers={['github']}
             supabaseClient={supabaseClient}
             appearance={{
                 theme: ThemeSupa,
