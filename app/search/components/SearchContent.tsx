beb48236d05ffd96123dfeca7631d5096eb2b027
mrwebwork <tech@allanswebwork.info>
2023-06-22 10:30:44 -0500

    Create like button for liked songs

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { LikeButton } from '@/components/LikeButton';
 import { MediaItem } from '@/components/MediaItem';
 
 import { Song } from '@/types'
@@ -37,7 +38,7 @@ export const SearchContent: React.FC<SearchContentProps> = ({
                         data={song}
                         />
                     </div>
-                    {/* //TODO: Add Like Button Here */}
+                    <LikeButton songId={song.id} />
                 </div>
             ))}
         </div>
