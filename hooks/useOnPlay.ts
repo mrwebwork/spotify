7b50d2320317eba062b6fd226759dc82cef38fca
mrwebwork <tech@allanswebwork.info>
2023-06-22 12:28:35 -0500

    Create useOnPlay hook

---
+++ hooks/useOnPlay.ts
@@ -0,0 +1,18 @@
+import { Song } from "@/types";
+import { usePlayer } from "./usePlayer";
+import { useAuthModal } from "./useAuthModal";
+import { useUser } from "./useUser";
+
+const useOnPlay = (songs: Song[]) => {
+    const player = usePlayer();
+    const authModal = useAuthModal();
+    const { user } = useUser();
+
+    const usePlay = (id: string) => {
+        if (!user) {
+            return authModal.onOpen();
+        }
+        player.setId(id);
+        player.setIds(songs.map((song) => song.id))
+    }
+}
