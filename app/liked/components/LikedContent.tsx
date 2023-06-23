7753c879935247c94e26867f84579509d39ff237
mrwebwork <tech@allanswebwork.info>
2023-06-22 23:15:01 -0500

    Pass onPlay to Liked Songs page

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -8,6 +8,7 @@ import { Song } from '@/types';
 import { useUser } from '@/hooks/useUser';
 import { MediaItem } from '@/components/MediaItem';
 import { LikeButton } from '@/components/LikeButton';
+import { useOnPlay } from '@/hooks/useOnPlay';
 
 interface LikedContentProps {
   songs: Song[];
@@ -16,6 +17,7 @@ interface LikedContentProps {
 export const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
   const router = useRouter();
   const { isLoading, user } = useUser();
+  const onPlay = useOnPlay(songs);
 
   useEffect(() => {
     if (!isLoading && !user) {
@@ -45,7 +47,7 @@ export const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
