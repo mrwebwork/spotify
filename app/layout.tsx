e9de74f9ab45cca7fe06eb8cf2e2a2099d61d03d
mrwebwork <tech@allanswebwork.info>
2023-07-03 17:28:14 -0500

    Upload favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,12 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   //* Providers & Components
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png" />
+      </head>
       <body className={font.className}>
         <ToasterProvider />
         <SupabaseProvider>
