379b8af3ca54355c0ce5bfb56b2d63cdfeaee3ef
mrwebwork <tech@allanswebwork.info>
2023-07-03 14:58:58 -0500

    Add error and loading state

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Box } from '@/components/Box';
+import { Triangle } from 'react-loader-spinner';
+
+const Loading = () => {
+  return (
+    <Box className="h-full flex items-center justify-content">
+      <Triangle
+        height="80"
+        width="80"
+        color="#22c55e"
+        ariaLabel="triangle-loading"
+        visible={true}
+      />
+    </Box>
+  );
+};
+
+export default Loading;
