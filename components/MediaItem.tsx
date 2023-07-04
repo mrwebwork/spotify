732fe5a6e10c2dcb451d307c848c5ac479e68038
mrwebwork <tech@allanswebwork.info>
2023-07-03 22:13:42 -0500

    Update MediaItem

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -6,19 +6,22 @@ import { useLoadImage } from '@/hooks/useLoadImage';
 
 import { Song } from '@/types';
 
+import { usePlayer } from '@/hooks/usePlayer';
+
 interface MediaItemProps {
   data: Song;
   onClick?: (id: string) => void;
 }
 
 export const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  const player = usePlayer();
   const imageUrl = useLoadImage(data);
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id);
     }
 
-    //TODO: Default turn on player
+    return player.setId(data.id);
   };
   return (
     <div
